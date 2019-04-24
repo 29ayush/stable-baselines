b9d642980cf16fa6eb1019c532b37342cd71f3d2
Ayush Mittal <29ayush@gmail.com>
2019-04-24 10:44:45 +0530

    fkme

--- stable_baselines/a2c/a2c.py
+++ stable_baselines/a2c/a2c.py
@@ -97,8 +97,10 @@ class A2C(ActorCriticRLModel):
 
             assert issubclass(self.policy, ActorCriticPolicy), "Error: the input policy for the A2C model must be an " \
                                                                 "instance of common.policies.ActorCriticPolicy."
-            self.delta = [0.055,0.06,0.065,0.07,0.075] * self.n_envs
+            #self.delta = [0.055,0.06,0.065,0.07,0.075] * self.n_envs
             #self.delta = 0
+            if type(self.delta) == list:
+                self.delta = self.delta * self.n_envs
             print("Model :- ",self.delta)
             self.graph = tf.Graph()
             with self.graph.as_default():
